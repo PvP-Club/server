2662f461e2d57ee7324937af0ee43c1bcffde143
Toby1710 <69259430+Toby1710@users.noreply.github.com>
2021-02-05 15:13:01 +0200

    Added cephalotus

--- color_nametags/init.lua
+++ color_nametags/init.lua
@@ -20,7 +20,7 @@ minetest.register_on_joinplayer(function(player)
   end
 end)
 
-local yellows = {"j45", "-lipop-", "RUBIUSOMG11", "minetest"}
+local yellows = {"j45", "-lipop-", "RUBIUSOMG11", "minetest", "cephalotus"}
 
 minetest.register_on_joinplayer(function(player)
   for _, n in pairs(yellows) do
