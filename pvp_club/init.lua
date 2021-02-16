bfae906efc3d3ec6107a2cb6987e6df9a91b78c3
Toby1710 <69259430+Toby1710@users.noreply.github.com>
2021-02-16 15:51:37 +0200

    Update init.lua

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -10,7 +10,7 @@ PVP.team_colors = {
     green = "#64f20b"
 }
 PVP.teams = {
-    blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Koda_"},
+    blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Koda_", "Gladius"},
     yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack"},
     green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank"}
 }
