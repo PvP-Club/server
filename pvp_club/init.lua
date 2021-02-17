93a6e7e02cdbe61c25b0ff52054f9bfe147ad38e
Avyukt More <65779812+moreavy@users.noreply.github.com>
2021-02-17 18:59:55 +0530

    Alignment

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -2,18 +2,20 @@
 local mt = minetest
 local ms = mt.get_mod_storage()
 PVP = {
-  players = {},
-  team_chat_enabled = true,
-  team_colors = {
-    blue = "#0000FF",
-    yellow = "#FFFF00",
-    green = "#64f20b"
+	players = {},
+  	team_chat_enabled = true,
+  	team_colors = {
+    	blue = "#0000FF",
+    	yellow = "#FFFF00",
+    	green = "#64f20b",
   },
+
   teams = {
     blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Koda_", "Gladius"},
     yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack"},
     green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank"}
   },
+	
   spawn = {
     r = 20,
     h = 20,
@@ -23,7 +25,9 @@ PVP = {
         y = 20,
         z = -28536
     },
+		
   },
+	
 }
 local dead_players = {}
 local immune_players = {}
