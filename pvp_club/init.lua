8e0d77883eff943bdaa58bd0db40d37ad690cd6e
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-03-06 12:27:04 +0200

    Removed Koda_ from whitelist

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -12,7 +12,7 @@ PVP.team_colors = {
 }
 PVP.teams = {
     red = {"clownwolf"},
-    blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Koda_", "Gladius", "liverpool"},
+    blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "liverpool"},
     yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack"},
     green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank"}
 }
