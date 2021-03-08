cdccd16692d32be95255a332f66907cbc583a163
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-03-08 17:35:44 +0200

    Add FranzJoseph to team red

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -11,7 +11,7 @@ PVP.team_colors = {
     red = "#e32727"
 }
 PVP.teams = {
-    red = {"clownwolf"},
+    red = {"clownwolf", "FranzJoseph"},
     blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "liverpool", "Xenon"},
     yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack"},
     green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank"}
