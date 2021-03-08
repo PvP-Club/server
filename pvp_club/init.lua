2035be714548c0eea47382643e1d95f717d102c8
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-03-08 11:41:56 +0200

    Add Xenon to blue team

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -12,7 +12,7 @@ PVP.team_colors = {
 }
 PVP.teams = {
     red = {"clownwolf"},
-    blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "liverpool"},
+    blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "liverpool", "Xenon"},
     yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack"},
     green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank"}
 }
