c354bf85b119bf5a4d08cca1514b8a0c422ff424
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-03-11 11:49:28 +0200

    Add smugler5 to blue team back

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -14,7 +14,7 @@ PVP.team_colors = {
 PVP.teams = {
     test = {"Test1"},
     red = {"clownwolf", "FranzJoseph", "Beta"},
-    blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "liverpool", "Xenon"},
+    blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "liverpool", "Xenon", "smugler5"},
     yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack"},
     green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank"}
 }
