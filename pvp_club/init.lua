d191514b21dde53aac2dacfff3723eb93d7beedf
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-03-22 13:17:55 +0200

    Add Bigfoot to yellow team

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -15,7 +15,7 @@ PVP.teams = {
     test = {"Test1"},
     red = {"clownwolf", "FranzJoseph", "Beta"},
     blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "liverpool", "Xenon", "smugler5"},
-    yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack", "Code-Sploit"},
+    yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack", "Code-Sploit", "Bigfoot45"},
     green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank"}
 }
 PVP.spawn = {
