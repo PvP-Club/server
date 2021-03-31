b7021fe329bc8053b5dca38752609ae1d41525f6
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-03-31 16:10:43 +0300

    Add N9Health to the team green

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -16,7 +16,7 @@ PVP.teams = {
     red = {"clownwolf", "FranzJoseph", "Beta"},
     blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "Xenon", "liverpool", "smugler5"},
     yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack", "Code-Sploit", "Bigfoot45"},
-    green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank"}
+    green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank", "N9Heatlh"}
 }
 PVP.spawn = {
     r = 20,
