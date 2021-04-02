d6057a95046567ca63c3ba254a03613b9ff7511f
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-04-02 12:01:01 +0300

    Add Rtx to team red (whitelist)

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -13,7 +13,7 @@ PVP.team_colors = {
 }
 PVP.teams = {
     test = {"Test1"},
-    red = {"clownwolf", "FranzJoseph", "Beta"},
+    red = {"clownwolf", "FranzJoseph", "Beta", "Rtx"},
     blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "Xenon", "liverpool", "smugler5"},
     yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack", "Code-Sploit", "Bigfoot45"},
     green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank", "N9Heatlh"}
