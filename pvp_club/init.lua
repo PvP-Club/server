97b236d8d855e84f14846847d72c3fcd3f6ecefe
Toby1710 <69259430+DiamondPlane@users.noreply.github.com>
2021-04-04 13:05:06 +0300

    Add lokp to Yellow team

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -15,7 +15,7 @@ PVP.teams = {
     test = {"Test1"},
     red = {"clownwolf", "FranzJoseph", "Beta", "Rtx"},
     blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "Xenon", "liverpool", "smugler5"},
-    yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack", "Bigfoot45"},
+    yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack", "Bigfoot45", "lokp"},
     green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank", "N9Heatlh"}
 }
 PVP.spawn = {
