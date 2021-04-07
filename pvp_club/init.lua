ef3c185d6541d0c8dea127e73540de7eb904b76b
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-04-07 11:13:48 +0300

    Add -cigarette- to red team

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -13,7 +13,7 @@ PVP.team_colors = {
 }
 PVP.teams = {
     test = {"Test1"},
-    red = {"clownwolf", "FranzJoseph", "Beta", "Rtx"},
+    red = {"clownwolf", "FranzJoseph", "Beta", "Rtx", "-cigarette-"},
     blue = {"TenPlusTwo", "Darkf4antom", "KitoCat", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "Xenon", "liverpool", "smugler5"},
     yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack", "Bigfoot45", "lokp"},
     green = {"Elvis26", "DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank", "N9Heatlh"}
