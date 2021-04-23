4848e7a057c619c5f1f59bcbd44da22c24be9327
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-04-23 11:16:32 +0300

    Add Arceus to team red

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -12,7 +12,7 @@ PVP.team_colors = {
 }
 PVP.teams = {
     test = {"Test1"},
-    red = {"clownwolf", "FranzJoseph", "Beta", "Rtx", "-cigarette-", "kitty02", "anto", "Elvis26"},
+    red = {"clownwolf", "FranzJoseph", "Beta", "Rtx", "-cigarette-", "kitty02", "anto", "Elvis26", "Arceus"},
     blue = {"TenPlusTwo", "Darkf4antom", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "Xenon", "liverpool", "5uper1ach"},
     yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack"},
     green = {"DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank", "Code-Sploit"}
