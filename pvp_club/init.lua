c3488393acbcac9d1fbf4c54c65b2170bdc09297
RyanZem <72454408+RyanZem@users.noreply.github.com>
2021-05-19 20:17:46 +0300

    Add winniepee0 to green team

--- pvp_club/init.lua
+++ pvp_club/init.lua
@@ -15,7 +15,7 @@ PVP.teams = {
     red = {"clownwolf", "FranzJoseph", "Beta", "Rtx", "-cigarette-", "kitty02", "anto", "Elvis26", "Arceus"},
     blue = {"TenPlusTwo", "Darkf4antom", "AnthonyDe", "SoulSeeker", "JediKnight", "Panquesito7", "Gladius", "Xenon", "liverpool", "5uper1ach", "PantsMan"},
     yellow = {"-lipop-", "minetest", "j45", "RUBIUSOMG11", "cephalotus", "Amine35", "realyg", "popidog_assaillant", "Elyas_Crack", "Luis_Mi"},
-    green = {"DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank", "Code-Sploit"}
+    green = {"DiamondPlane", "gameit", "end", "Skyisblue", "-CrocMoney-", "N4xQ", "LuaFrank", "Code-Sploit", "winniepee0"}
 }
 PVP.spawn = {
     r = 20,
